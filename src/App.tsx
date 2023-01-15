a31353309ea95603f2fb35807e359d59e15147dc
Manqoba <manqobazwane22@gmail.com>
2023-01-15 21:56:30 +0200

    HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Style/App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {LinkContainer} from 'react-router-bootstrap';
+import  Container from 'react-bootstrap/Container';
+import  Navbar from 'react-bootstrap/Navbar';
+import { Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header >
+          <Navbar bg="white" variant="white">
+            <Container style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
+              <LinkContainer style={{display: 'block'}} to="/">
+                <Navbar.Brand className='nav'>
+                  earbeats
+                </Navbar.Brand>
+              </LinkContainer>
+              <Container style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}}>
+              <LinkContainer to="/">
+                <Nav.Link className='nav'>
+                  Earbuds
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/">
+                <Nav.Link className='nav'>
+                  Headphones
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/">
+                <Nav.Link className='nav'>
+                  Accessories
+                </Nav.Link>
+              </LinkContainer>
+              </Container>
+            </Container>
+          </Navbar>
+        </header>
+        <div className="product">
+          <Container>
+            <span id='heading'>Senneiser</span>
+            <h1 id='sub-heading'>Wireless N7007</h1>
+            <span id='product-details'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </span>
+            <LinkContainer to="/">
+              <span id='link-product'>EXPLORE PRODUCT</span>
+            </LinkContainer>
+          </Container>    
+        </div>
+        <div>
+            <Container className='gradient-background'>
+              
+            </Container>
+        </div>
+        <div className='paginator'>
+          <span>01/05</span>
+        </div>
+        <Container>
+          <footer className='foot-bs'>
+              All rights reserved    
+          </footer>
+        </Container>
+      </div>
+    </BrowserRouter>
   );
 }
 
