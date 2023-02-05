4495dd65b948729ba5e4596b23f763f090d0ba75
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-02-05 21:58:18 +0100

    progress: Made headernav mobile responsive

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, MouseEvent } from 'react'
 import { NavLink } from 'react-router-dom';
 import '../Style/headerNav.css';
 import { ArrowUpRightIcon, ChartBarIcon } from '@heroicons/react/24/solid'
@@ -7,6 +7,7 @@ import GsapAnimations from '../utils/gsapAnimations'
 
 function HeaderNav() {
     const animation = useRef<GsapAnimations | null>(null);
+
     const handleBackClick = () => {
         if (animation.current?.currentAnimationPage === 'ProductDetails') {
             // The order matters
@@ -23,22 +24,30 @@ function HeaderNav() {
             animation.current.productCompactment.hideComponent();
         }
     }
+
+    const handleHamburgerClick = (e: MouseEvent<HTMLDivElement>): void => {
+        if (animation.current?.headerNav.disableAnimation) return;
+        const isOpen = e.currentTarget.getAttribute("data-isopen");
+
+        animation.current?.headerNav.hamburgerClick(isOpen === "true" ? true : false);
+        e.currentTarget.setAttribute("data-isopen", isOpen === "true" ? "false" : "true");
+    }
     useEffect(() => {
         animation.current = new GsapAnimations();
 
         return () => animation.current?.dispose();
     }, [])
     return (
-        <header>
-            <nav className='nav_wrapper'>
-                <ul>
-                    <li className='nav_list_logo'>
-                        <NavLink to={'/'}>
-                            <ChartBarIcon className='icon' />
-                            <span>earbeats</span>
+        <header className='hover-effect reset'>
+            <div className='nav_list_logo'>
+                <NavLink to={'/'}>
+                    <ChartBarIcon className='icon' />
+                    <span>earbeats</span>
 
-                        </NavLink>
-                    </li>
+                </NavLink>
+            </div>
+            <nav className='nav_wrapper web'>
+                <ul>
                     <li className='nav_list_link hover-effect'>
                         <NavLink to={'/'}>
                             Earbuds
@@ -54,14 +63,38 @@ function HeaderNav() {
                             Accessories
                         </NavLink>
                     </li>
-                    <li onClick={handleBackClick} className='hover-effect arrowed_button nav_list_Back '>
-                        <Link to="/">
-                            <span>Back</span>
-                            <ArrowUpRightIcon className='icon' />
-                        </Link>
+                </ul>
+            </nav>
+            <nav className='nav_wrapper mobile'>
+                <div data-isopen="false" onClick={handleHamburgerClick} className='hamburger'>
+                    <span className='hamburgerFirst'></span>
+                    <span className='hamburgerSecond'></span>
+                    <span className='hamburgerThird'></span>
+                </div>
+                <ul>
+                    <li className='nav_list_link mobile'>
+                        <NavLink to={'/'}>
+                            Earbuds
+                        </NavLink>
+                    </li>
+                    <li className='nav_list_link mobile'>
+                        <NavLink to={'/'}>
+                            Headphones
+                        </NavLink>
+                    </li>
+                    <li className='nav_list_link mobile'>
+                        <NavLink to={'/'}>
+                            Accessories
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
+            <div onClick={handleBackClick} className='hover-effect arrowed_button nav_list_Back '>
+                <Link to="/">
+                    <span>Back</span>
+                    <ArrowUpRightIcon className='icon' />
+                </Link>
+            </div>
         </header>
     )
 }
--- src/utils/gsapAnimations/HeaderNavAnimation.ts
+++ src/utils/gsapAnimations/HeaderNavAnimation.ts
@@ -3,12 +3,78 @@ import Animation from ".";
 
 export default class HeaderNav {
     private animation: Animation;
+    disableAnimation: boolean
     constructor(animation: Animation) {
-        this.animation = animation
+        this.animation = animation;
+        this.disableAnimation = false;
+    }
+
+    hamburgerClick(isOpen: boolean) {
+        this.disableAnimation = true;
+        const firstLine = document.querySelector(".hamburgerFirst") as HTMLSpanElement;
+        const secondLine = document.querySelector(".hamburgerSecond") as HTMLSpanElement;
+        const thirdLine = document.querySelector(".hamburgerThird") as HTMLSpanElement;
+        const navWrapper = document.querySelector(".nav_wrapper.mobile ul") as HTMLElement;
+
+        const together = [firstLine, secondLine, thirdLine];
+        if (isOpen) { // then close it
+            const tl = gsap.timeline({ defaults: { duration: 0.2 } });
+            tl.to(firstLine,
+                {
+                    rotate: "0"
+                },
+                "rotate"
+            ).to(thirdLine,
+                {
+                    rotate: "-0"
+                },
+                "rotate"
+            ).to(secondLine,
+                {
+                    scaleY: 1
+                }
+            ).to(together,
+                {
+                    scaleX: 0.5
+                }
+            ).to(navWrapper,
+                {
+                    scaleY: 0,
+                    onComplete: () => { this.disableAnimation = false }
+                }
+            )
+
+        } else { // open it
+            const tl = gsap.timeline({ defaults: { duration: 0.2 } });
+            tl.to(together,
+                {
+                    scaleX: 1
+                }
+            ).to(secondLine,
+                {
+                    scaleY: 0
+                }
+            ).to(firstLine,
+                {
+                    rotate: "40deg"
+                },
+                "rotate"
+            ).to(thirdLine,
+                {
+                    rotate: "-40deg"
+                },
+                "rotate"
+            ).to(navWrapper,
+                {
+                    scaleY: 1,
+                    onComplete: () => { this.disableAnimation = false }
+                }
+            )
+        }
     }
 
     hideNavLinks() {
-        const navLinks = document.querySelectorAll('.nav_list_link')!;
+        const navLinks = document.querySelectorAll('.nav_wrapper')!;
         const navLinkBack = document.querySelectorAll('.nav_list_Back')!;
 
         const tl = gsap.timeline({ defaults: { duration: 1, } });
@@ -34,7 +100,7 @@ export default class HeaderNav {
     }
 
     showNavLinks() {
-        const navLinks = document.querySelectorAll('.nav_list_link')!;
+        const navLinks = document.querySelectorAll('.nav_wrapper')!;
         const navLinkBack = document.querySelectorAll('.nav_list_Back')!;
 
         const tl = gsap.timeline({ defaults: { duration: 1, } });
